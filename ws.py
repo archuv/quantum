dc53696e4911f581619980a46eea8d03ba3a8b53
Gias Uddin <guddin93@gmail.com>
2013-05-09 01:40:51 +0600

    replace subprocess and use python for editing file

--- ws.py
+++ ws.py
@@ -1,8 +1,11 @@
-import subprocess
+#
+
+import os, sys, subprocess
 
 def main():
     print "installing Mysql ...."
-    #subprocess.call(["zypper", "install", "mysql-community-server"])[it demand reboot]
+    #subprocess.call(["zypper", "install", "mysql-community-server"])
+    #subprocess.call(["reboot"])
     #subprocess.call(["zypper", "install", "postfix", "postfix-mysql", "mysql-community-server"])
     #subprocess.call(["systemctl","enable", "mysql.service"])
     #subprocess.call(["rcmysql", "reload"])
@@ -14,14 +17,40 @@ def main():
     print "installing mod_php5..........."
     #subprocess.call(["zypper", "install", "apache2-mod_php5"])
     #subprocess.call(["rcapache2", "reload"])
-    #subprocess.call(["vi", "/srv/www/htdocs/info.php"])
-    subprocess.call(["rcapache2", "reload"])
+    file_info_php = open('/srv/www/htdocs/info.php', 'w')
+    info_code = '<?php\nphpinfo();\n?>'
+    file_info_php.write(info_code)
+    #subprocess.call(["rcapache2", "reload"])
     print "installing phpMyAdmin.............."
-    subprocess.call(["zypper", "install", "phpMyAdmin"])
-    subprocess.call(["rcapache2", "reload"])
+    #subprocess.call(["zypper", "install", "phpMyAdmin"])
+    #subprocess.call(["rcapache2", "reload"])
+    #subprocess.call(["a2enmod", "rewrite"])
+    print "virtual host creating................."
+    #subprocess.call(["vi", "/etc/apache2/listen.conf"])
+    #subprocess.call(["vi", "/etc/hosts"])
+    #subprocess.call(["/etc/apache2", "ls"])
     
+def cmdtest(printLine):
+    #subprocess.call(["vi", "/etc/apache2/listen.conf"])
+    f_in = open ("/etc/apache2/listen.conf", 'r')
+    f_out = open ("/etc/apache2/listen.conf", 'w')
     
-  
+    lines = f_in.readlines();
+    lines_out = lines
+        
+    if lines[40].strip() == '#NameVirtualHost *:80': 
+        print lines[40].strip() if printLine == True else None
+        print 'match'
+        
+        
+        lines[40].replace = 'NameVirtualHost *:80'
+        print lines[40]
+    else:
+        print lines[40].strip() if printLine == True else None
+        print 'voool'
+
+
 if __name__ == "__main__":
-    main()
-    
+    #main()
+    cmdtest(False)
+
